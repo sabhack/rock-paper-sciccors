10166341d7341c576b6fc8dca051404928ccb560
Sabhack <mi.sabhack@gmail.com>
2022-04-11 12:23:12 +0100

    Wrote a function to play a single round game

---
+++ script.js
@@ -0,0 +1,36 @@
+// Computer options
+const options = ['rock', 'paper', 'scissors'];
+
+// Computer selection of random value from the 3 options
+function computerPlay(randomChoice) {
+    randomChoice = options[Math.floor(Math.random()*3)];
+    return randomChoice;
+}
+
+// Outputting Computer selection
+console.log(computerPlay(options));
+
+// Function that Plays a Single Round of Rock Paper Scissors.
+function playRound(playerSelection, computerSelection) {
+    // This case the Player wins
+    if (    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+            (playerSelection === 'paper' && computerSelection === 'rock') || 
+            (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        return `Computer Lose! ${playerSelection} beats ${computerSelection}`; 
+    }
+    else if ( (computerSelection === 'rock' && playerSelection === 'scissors') ||
+              (computerSelection === 'paper' && playerSelection === 'rock') ||
+              (computerSelection === 'scissors' && playerSelection === 'paper')
+    ) {
+        return `Player Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    else {
+        return `The Game is Tie Player selected ${playerSelection} and Computer selected ${computerSelection}`;
+    }
+}
+
+let playerSelection = "rock";
+playerSelection.toLocaleLowerCase();
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
